60e3f0d56d8de4c876618c46cc851b7889dc294e
sanddev8 <bitu_sandeep@yahoo.co.in>
2017-09-03 20:04:26 -0400

    NewTest.Java file has submit

--- WebDriverTest1/src/test/java/example/NewTest.java
+++ WebDriverTest1/src/test/java/example/NewTest.java
@@ -15,7 +15,7 @@ public class NewTest {
   public void f() {
   driver.findElement(By.xpath("//*[@name='userName']")).sendKeys("tutorial");
   driver.findElement(By.xpath("//*[contains(@name,'password')]")).sendKeys("tutorial");
-  driver.findElement(By.xpath("//*[contains(@name,'login')]")).click();
+  driver.findElement(By.xpath("//*[contains(@name,'login')]")).submit();
   Assert.assertEquals("Find a Flight: Mercury Tours:", driver.getTitle());
   }
   @BeforeTest
